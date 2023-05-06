6bd7561ddcb4db8eb90d5a2e6fb1565386bb34f7
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-05-06 11:12:45 -0500

    added 3 query prepared statements

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -23,11 +24,40 @@ const db = mysql.createConnection({
 // WHEN I choose to view all departments
 // THEN I am presented with a formatted table showing department names and department ids
 
+function viewDepartments() {
+    db.query('SELECT * FROM department', (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+    });
+};
+viewDepartments();
+
 // WHEN I choose to view all roles
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+// function viewRoles() {
+//     db.query('SELECT * FROM employee_role', (err, results) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.table(results);
+//     });
+// }
+// viewRoles();
+
 
 // WHEN I choose to view all employees
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+// function viewEmployees() {
+//     db.query('SELECT * FROM employee', (err, results) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.table(results);
+//     });
+// }
+// viewEmployees();
 
 // WHEN I choose to add a department
 // THEN I am prompted to enter the name of the department and that department is added to the database
